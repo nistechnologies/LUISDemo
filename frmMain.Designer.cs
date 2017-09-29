39a86f5eb226ecf5858510e6a36c313d4b4da3fb
nistechnologies <31288324+nistechnologies@users.noreply.github.com>
2017-09-29 16:30:07 -0400

    Add files via upload
    V1 of demo app, using car dealer as an example.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Demo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(94, 72);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(94, 118);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(550, 211);
+            this.tbOutput.TabIndex = 1;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(248, 29);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(339, 22);
+            this.tbSearch.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "What can I do for you?";
+            this.label1.UseMnemonic = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmMain";
+            this.Text = "LUIS Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Demo.Models;
+
+
+namespace Demo
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            tbSearch.Text = "find a mustang";            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var result =  LUISParse(tbSearch.Text);
+        }
+
+        private async Task<string> LUISParse(string query)
+        {
+            string topIntent = string.Empty;
+            string topEntity = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://eastus2.api.cognitive.microsoft.com/luis/v2.0/apps/04bf5081-2934-4bcb-ad4e-69f801a91bfc?subscription-key=fcc211a43a2d4a78a90e83f23ecebc6d&verbose=true&timezoneOffset=0&q=" + query;
+
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var response = JsonConvert.DeserializeObject<LUISResponse>(jsonResponse);
+                    topIntent = response.intents[0].intent;
+                    topEntity = response.entities[0].entity;                    
+                }
+            }
+
+            switch (topIntent)
+            {
+                case ("FindCar"):
+                    Car car = new Car();
+                    tbOutput.Text = car.Get(topEntity);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return topEntity;
+        }
+
+
+    }
+}
